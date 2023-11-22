b789339d6239065c41f417eb7ee03de1f3c761e4
DidasMba <daniel.mbarushi@gmail.com>
2023-11-23 00:31:07 +0300

    finished

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,46 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+    print("Happy New Year!")
+
+# Call the function
+happy_new_year()
+
+    
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    squared_list = [x**2 for x in int_list]
+    return squared_list
+
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
+# Call the function
+fizzbuzz()
+
+    
+
+def reverse_string(s):
+    reversed_str = ""
+    for char in s:
+        reversed_str = char + reversed_str
+    return reversed_str
+
+def square_integers(int_list):
+    return [num**2 for num in int_list]
+
